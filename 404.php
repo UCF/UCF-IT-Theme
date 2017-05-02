0696682ddb2cd49c7d62b8b383812901840db19a
thedirged <joe.raditch@ucf.edu>
2017-05-02 14:15:40 -0400

    404 fix

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@
 get_header(); ?>
 
 <div class="wrap">
-	<script>alert("404 - This page cannot be found!");</script>
+	
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
