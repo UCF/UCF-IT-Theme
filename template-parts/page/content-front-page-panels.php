db0d3fa3d9dcca46fb84c7c9e16eee43145c36a1
thedirged <joe.raditch@ucf.edu>
2017-05-02 15:37:17 -0400

    remove titles

--- template-parts/page/content-front-page-panels.php
+++ template-parts/page/content-front-page-panels.php
@@ -29,10 +29,11 @@ global $twentyseventeencounter;
 
 	<div class="panel-content">
 		<div class="wrap">
+			<!--
 			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
-				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+			<?php twentyseventeen_edit_link( get_the_ID() ); ?>
 
 			</header><!-- .entry-header -->
 
