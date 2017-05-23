f50b995845abe773d24b4516bb4cdcbfc2a8e3fc
thedirged <joe.raditch@ucf.edu>
2017-05-23 10:56:05 -0400

    site infor update

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	&copy; UCF IT | <a href="http://www.ucf.edu">University of Central Florida</a>
+	&copy; UCF IT | <a href="http://www.ucf.edu">University of Central Florida</a> | <a href="http://ucf.qualtrics.com/jfe/form/SV_1Fx1yHS6myqzwSF"> Site Feedback</a>
 </div><!-- .site-info -->
